8b5b4c3f846180fc59164fb6cf1876e8a767621a
Nikhil Hade <nikhilhade21@gmail.com>
2024-08-02 23:26:58 +0530

    ddd

--- Server/index.js
+++ Server/index.js
@@ -1,11 +1,14 @@
 const express = require("express")
 const mongoose = require('mongoose')
 const cors = require('cors')
+const UserModel = require('./Modules/Users')
 
 const app = express()
 app.use(cors())
 app.use(express.json())
 const PORT = 3001
+
+
 
 app.listen(PORT, () => {
     console.log("Server is running on " + PORT);
