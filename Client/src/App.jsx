24cf44d903ea967577f4b4b7555605754b49dcec
Nikhil Hade <nikhilhade21@gmail.com>
2024-08-04 23:02:12 +0530

    sdw

--- Client/src/App.jsx
+++ Client/src/App.jsx
@@ -17,7 +17,7 @@ function App() {
        <Routes>
         <Route path='/' element={<User />}></Route>
         <Route path='/create' element={<CreateUser />}></Route>
-        <Route path='/Update' element={<UpdateUser /> }></Route>
+        <Route path='/Update/:id' element={<UpdateUser /> }></Route>
        </Routes>
      </BrowserRouter>
     </div>
