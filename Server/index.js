5a0c8ec92b49ea729328601468b5ed5ab018666e
Nikhil Hade <nikhilhade21@gmail.com>
2024-08-04 21:03:38 +0530

    added fnc

--- Server/index.js
+++ Server/index.js
@@ -6,17 +6,22 @@ const UserModel = require('./Modules/Users')
 const app = express()
 app.use(cors())
 app.use(express.json())
-const PORT = 3001
 
 //db connection
 mongoose.connect("mongodb://127.0.0.1:27017/crud")
 
-app.post('/createUser', (req, res) => {
-    UserModel.create(req, body)
+app.get('/', (req, res) => {
+    UserModel.find({})
     .then(users => res.json(users))
     .catch(err => res.json(err))
 })
 
-app.listen(PORT, () => {
-    console.log("Server is running on " + PORT);
+app.post("/createUser", (req, res) => {
+    UserModel.create(req.body)
+    .then(users => res.json(users))
+    .catch(err => res.json(err))
+})
+
+app.listen(3001, () => {
+    console.log("Server is running ");
 })
