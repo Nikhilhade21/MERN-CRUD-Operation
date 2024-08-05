12f98b69223e6f24c2901ae56c849bf2b7a2c411
Nikhil Hade <nikhilhade21@gmail.com>
2024-08-05 14:04:26 +0530

    delete function

--- Client/User.jsx
+++ Client/User.jsx
@@ -11,6 +11,12 @@ const User = () => {
     .catch(err => console.log(err))
   },[])
 
+  const handleDelete = (e) => {
+    axios.delete('/deleteUser/'+id)
+    .then(res => console.log(res))
+    .catch(err => console.log(err))
+  }
+
   return (
     <div className='d-flex vh-100 bg-primary justify-content-center align-items-centre '>
       <div className='w-50 bg-white rounded p-3'>
@@ -33,7 +39,7 @@ const User = () => {
                   <td>{user.age}</td>
                   <td>
                     <Link to={`/update/${user._id}`} className='btn btn-success'>Update</Link> 
-                    <button className='btn btn-danger'>Delete</button>
+                    <button className='btn btn-danger' onClick={(e) => handleDelete(user._id)}>Delete</button>
                   </td>
                 </tr>
               }) 
